9c4349377550ca0de689c016bf2a7911e3792197
Aidas <aidas.pu@gmail.com>
2020-10-03 13:15:04 +0300

    Initial commit

---
+++ function/calculator.py
@@ -0,0 +1,17 @@
+class Calculator:
+
+    def add(self, a, b):
+        return a + b
+
+    def substract(self, a, b):
+        return a - b
+
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        return a / b
+
+    def power(self, a, n):
+        return pow(a, n)
---
+++ tests/unit_test/test_calculator.py
@@ -0,0 +1,43 @@
+from function.calculator import Calculator
+import pytest
+
+@pytest.fixture()
+def answer():
+    return 4
+
+
+
+def test_addition(answer):
+    calculator = Calculator()
+    assert calculator.add(2, 2) == answer
+
+def test_addition_with_char_exception():
+    calculator = Calculator()
+    with pytest.raises(Exception):
+        assert calculator.add("three", 2)
+
+def test_substraction(answer):
+    calculator = Calculator()
+    assert calculator.substract(6, 2) == answer
+
+def test_sbustraction_wrong_value():
+    calculator = Calculator()
+    with pytest.raises(Exception):
+        assert calculator.substract("test", 2) == 0
+
+
+def test_multiplication(answer):
+    calculator = Calculator()
+    assert calculator.multiply(2, 2) == answer
+
+def test_division(answer):
+    calculator = Calculator()
+    assert calculator.divide(8, 2) == answer
+
+def test_raise_by_power():
+    calculator = Calculator()
+    assert calculator.power(10, 2) == 100
+
+
+
+
